52b66fe9296c8990b61168d94470fc49e619494d
Roland van Laar <roland@micite.net>
2016-02-01 14:29:26 +0100

    Add shebang and import fix.

--- staan_op.py
+++ staan_op.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python2.7
+
 from time import sleep
 from mcpi.minecraft import Minecraft
-from .blocks import get_block_name
+import blocks as mcx_blocks
 
 
 def staan_op_blok():
     pos = mc.player.getTilePos()
     blok_id = mc.getBlock(pos.x, pos.y - 1, pos.z)
-    blok_naam = get_block_name(blok_id)
+    blok_naam = mcx_blocks.get_block_name(blok_id)
     mc.postToChat("Ik sta op %s" % (blok_naam, ))
 
 if __name__ == '__main__':
     mc = Minecraft.create()
-    while True:
+    i = 0
+    while 1 < 60:
         staan_op_blok(mc)
-        sleep(1)
+        sleep(2)
